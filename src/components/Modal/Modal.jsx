6a24b5f1a984ec99e4200c6ca11169ca0c19f4aa
BielovaIryna <belova83@gmail.com>
2023-11-13 15:45:41 +0200

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+
+import { useEffect } from 'react';
 import css from './Modal.module.css'
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeEsc);
-    document.body.style.overflow = 'hidden';
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeEsc);
-    document.body.style.overflow = 'auto';
-  }
-  closeEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.closeModal();
-    }}
-    handleOverayClick = e => {
+export const Modal = ({closeModal, imageData}) => {
+  useEffect(()=>{
+    const closeEsc = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }}
+      window.addEventListener('keydown', closeEsc);
+      document.body.style.overflow = 'hidden';
+      return () =>{window.removeEventListener('keydown', closeEsc);
+    document.body.style.overflow = 'auto';}
+  }, [closeModal])
+
+   const    handleOverayClick = e => {
       if (e.target === e.currentTarget) {
-        this.props.closeModal();
+        closeModal();
       }
     };
   
-  render() {
+  
     return (
-      <div className={css.overlay} onClick={this.handleOverayClick}>
+      <div className={css.overlay} onClick={handleOverayClick}>
         <div className={css.modal}>
-          <img src={this.props.imageData.name} alt={this.props.imageData.id} />
+          <img src={imageData.name} alt={imageData.id} />
         </div>
       </div>
     );
-  }
+  
 }
